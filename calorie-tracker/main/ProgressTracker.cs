805cc970ce7e69065d752b649f24199d400e8214
Sajad Bayram <sajad.bayram@learnet.se>
2025-02-27 11:10:15 +0100

    jag gjorde klart alla klasser och connecta de

---
+++ calorie-tracker/main/ProgressTracker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calorie_tracker.main
+{
+    public class ProgressTracker
+    {
+        //hämta info
+        public UserProfile User { get; set; }
+
+
+        // en konsruktor som tar in user data some history och mål
+        public ProgressTracker(UserProfile user)
+        {
+            User = user;
+        }
+
+    }
+
+}
