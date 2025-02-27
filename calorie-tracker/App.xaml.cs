ce05c5965f03880fe6912ba57ba308323f97ec04
sajad.bayram <sajad.bayram@5CD2226SV0>
2025-02-27 09:52:15 +0100

    Add project files.

---
+++ calorie-tracker/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace calorie_tracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
